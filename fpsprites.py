fa385aa6a8c34ec3010279d373504e787cbdc836
benmaya05 <benmaya05@icloud.com>
2023-12-07 09:30:44 -0800

    worked on countries list

--- fpsprites.py
+++ fpsprites.py
@@ -1,1 +1,28 @@
 # This file was created by: Ben Maya on 12/1/2023
+
+# https://www.britannica.com/topic/list-of-countries-1993160
+
+countries = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola",
+            "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria",
+            "Azerbaijan", "The Bahamas", "Bahrain", "Bangladesh", "Barbados",
+            "Belarus", "Belgium", "Belize", "Benin", "Bhutan"
+            "Bolivia", "Bosnia and Herzegovina", "Bostwana", "Brazil", "Brunei"
+            "Bulgaria", "Burkina Faso", "Burundi", "Cabo Verde", "Cambodia",
+            "Cameroon", "Canada", "Central African Republic", "Chad", "Chile",
+            "China", "Colombia", "Comoros", "Democratic Republic of the Congo", "Republic of the Congo",
+            "Costa Rica", "Cote d'Ivoire", "Croatia", "Cuba", "Cyprus",
+            "Czech Republic", "Denmark", "Djibouti", "Dominica", "Dominican Republic",
+            "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea",
+            "Eritrea", "Estonia", "Eswatini", "Ethiopia", "Fiji",
+            "Finland", "France", "Gabon", "The Gambia", "Georgia",
+            "Germany", "Ghana", "Greece", "Grenada", "Guatemala",
+            "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras",
+            "Hungary", "Iceland", "India", "Indonesia", "Iran",
+            "Iraq", "Ireland", "Israel", "Italy", "Jamaica",
+            "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati",
+            "North Korea", "South Korea", "Kosovo", "Kuwait", "Kyrgyzstan",
+            "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia",
+            "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar",
+            "Malawi", "Malaysia", "Maldives", "Mali", "Malta"
+
+             ]
